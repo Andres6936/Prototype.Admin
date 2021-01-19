437b3efcd59cc227d64e711371516cf7c1a2831c
Joan Andrés <andres6936@live.com>
2021-01-19 10:01:09 -0500

    Connection with Mongo

--- index.js
+++ index.js
@@ -2,8 +2,31 @@
 
 const express = require('express');
 const path = require('path');
+const {MongoClient} = require('mongodb')
+
 const app = express();
 const port = 3000;
+
+// Connection URI
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        // Connect the client to server
+        await client.connect();
+
+        // Establish and verify connection
+        await client.db("admin").command({ping: 1});
+        console.log("Connected successfully to server");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
