d295ace6d60a4789fbac6734f63ad500d6724588
Joan Andrés <andres6936@live.com>
2021-01-20 15:09:03 -0500

    Try post methods

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 "use strict";
 
+const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const express = require('express');
 const path = require('path');
 const {MongoClient} = require('mongodb')
+//
+// // Connection URI
+// const uri = "mongodb://localhost:27017";
+//
+// const client = new MongoClient(uri);
+//
+// async function run() {
+//     try {
+//         // Connect the client to server
+//         await client.connect();
+//
+//         // Establish and verify connection
+//         const databse = client.db("admin");
+//         const collection = databse.collection("users");
+//
+//         console.log("Connected successfully to server");
+//
+//         return collection;
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+//
+// run().catch(console.dir);
 
 const app = express();
 const port = 3000;
@@ -21,6 +47,14 @@ app.get('/html/:file', (req, res) => {
     res.render(path.join(__dirname + '/public/html/' + req.params.file));
 })
 
+app.post('/html/register.html', (req, res) => {
+    console.log(req.body);
+})
+
+// support json encoded bodies
+app.use(bodyParser.json())
+// support encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public/'));
 app.set('view engine', 'html');
 
--- js/database/connection.js
+++ js/database/connection.js
@@ -1,22 +1,3 @@
 "use strict";
 
-// Connection URI
-const uri = "mongodb://localhost:27017";
 
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-        // Connect the client to server
-        await client.connect();
-
-        // Establish and verify connection
-        await client.db("admin").command({ping: 1});
-        console.log("Connected successfully to server");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
