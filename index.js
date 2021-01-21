68fc1d1a0df53bb6eafda09b43b8ec4dc01cf2f9
Joan Andrés <andres6936@live.com>
2021-01-20 21:18:48 -0500

    Fixed problem express throws error as `body-parser deprecated undefined extended`

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ nunjucks.configure('public/html', {
 });
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(express.static('public/'));
